1f0c0af88c0a0d4063ac2f6af0a5e728a76845e2
Mtnquang <71747330+Mtnquang@users.noreply.github.com>
2020-11-05 13:38:43 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb://localhost:27017/shopbook',
+        dbname : "shopbook",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb://localhost:27017/shopbook',
+        dbname : "shopbook",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
